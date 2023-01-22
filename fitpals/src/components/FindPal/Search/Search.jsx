5ff6bb45123a0ee4408cdcc31b704cb45fbdf2c6
Maciej Walentynowicz <maciej.walentynowicz@gmail.com>
2023-01-22 14:51:27 +0100

    made some css changes to search forms

--- fitpals/src/components/FindPal/Search/Search.jsx
+++ fitpals/src/components/FindPal/Search/Search.jsx
@@ -2,23 +2,47 @@ import styles from "./Search.module.css";
 
 export const Search = () => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <form className={styles.formStyling}>
         <h2 className={styles.formHeading}>Wyszukaj swojego FitPala !</h2>
         <div className={styles.forms}>
-          <label htmlFor="date">Podaj datę aktywności:</label>
-          <input type="date" name="date" id="date" />
-          <input type="time" name="time" id="time" />
+          <label htmlFor="date" className={styles.titles}>
+            Podaj datę aktywności:
+          </label>
+          <input
+            type="date"
+            name="date"
+            id="date"
+            className={`${styles.formsInput} ${styles.dateInput}`}
+          />
+          <input
+            type="time"
+            name="time"
+            id="time"
+            className={`${styles.formsInput} ${styles.timeInput}`}
+          />
         </div>
         <div className={styles.forms}>
-          <label htmlFor="city">Podaj miejscowość:</label>
-          <input type="text" name="city" id="city" />
+          <label htmlFor="city" className={styles.titles}>
+            Podaj miejscowość:
+          </label>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            value="Miejscowość"
+            className={`${styles.formsInput} ${styles.cityInput}`}
+          />
         </div>
-        <div>
-          <label htmlFor="activity" className={styles.forms}>
+        <div className={styles.selectionInput}>
+          <label htmlFor="activity" className={styles.titles}>
             Wybierz rodzaj aktywności:
           </label>
-          <select name="activity" id="activity">
+          <select
+            name="activity"
+            id="activity"
+            className={`${styles.formsInput} ${styles.activityInput}`}
+          >
             <option value="Boks">Boks</option>
             <option value="Fitness">Fitness</option>
             <option value="Piłka Nożna">Piłka Nożna</option>
@@ -37,6 +61,6 @@ export const Search = () => {
         </div>
         <input type="submit" value="Wyszukaj" className={styles.submit} />
       </form>
-    </>
+    </div>
   );
 };
