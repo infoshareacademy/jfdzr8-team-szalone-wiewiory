b9cc66f7cd43b11b1771a7bf983a2e18c5ab1b8d
Adam Pawlowski <101446507+AdamPawlowski84@users.noreply.github.com>
2023-02-06 23:00:03 +0100

    Add tabs in YourActivity module

--- fitpals/src/components/YourActivity/YourActivity.jsx
+++ fitpals/src/components/YourActivity/YourActivity.jsx
@@ -1,13 +1,62 @@
 import { FormActivity } from "./FormActivities/FormActivities";
 import { CreatedActivities } from "./CreatedActivities/CreatedActivities";
 import { JoinedActivities } from "./JoinedActivities/JoinedActivities";
+import { useState } from "react";
+import styles from "./YourActivity.module.css";
 
 export const YourActivity = () => {
+  const [currentTab, setCurrentTab] = useState("1");
+  const tabs = [
+    {
+      id: 1,
+      tabTitle: "Utwórz Aktywność",
+      content: <FormActivity />,
+    },
+    {
+      id: 2,
+      tabTitle: "Aktywności Uworzone",
+      content: <CreatedActivities />,
+    },
+
+    {
+      id: 3,
+      tabTitle: "Dołączono",
+      content: <JoinedActivities />,
+    },
+  ];
+
+  const handleTabClick = (e) => {
+    setCurrentTab(e.target.id);
+  };
+
   return (
     <>
-      <FormActivity />
-      <CreatedActivities />
-      <JoinedActivities />
+      <div className={styles.container}>
+        <div className={styles.tabs}>
+          {tabs.map((tab, i) => (
+            <button
+              key={i}
+              id={tab.id}
+              disabled={currentTab === `${tab.id}`}
+              onClick={handleTabClick}
+            >
+              {tab.tabTitle}
+            </button>
+          ))}
+        </div>
+        <div className={styles.content}>
+          {tabs.map((tab, i) => (
+            <div key={i}>
+              {currentTab === `${tab.id}` && (
+                <div>
+                  <p className={styles.title}></p>
+                  <p>{tab.content}</p>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
