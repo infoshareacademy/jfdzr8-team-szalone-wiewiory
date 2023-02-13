e3f3cbcbe0821c06e34bb661692bb5e5764d57ac
Maciej Walentynowicz <maciej.walentynowicz@gmail.com>
2023-02-13 18:33:25 +0100

    made changes to errors

--- fitpals/src/components/Authorization/ForgotPassword.jsx
+++ fitpals/src/components/Authorization/ForgotPassword.jsx
@@ -2,9 +2,12 @@ import { sendPasswordResetEmail } from "@firebase/auth";
 import { auth } from "../../api/firebase";
 import { getFormData } from "./getFormData";
 import { Form } from "../Authorization/Form";
-import { firebaseErrors } from "../Authorization/firebaseErrors";
+// import { firebaseErrors } from "../Authorization/firebaseErrors";
+import { RegisterModal } from "../Modals/RegisterModal/RegisterModal";
+import { useState } from "react";
 
 export const ForgotPassword = () => {
+  const [show, setShow] = useState(false);
   const handlePasswordReset = (e) => {
     e.preventDefault();
 
@@ -12,15 +15,19 @@ export const ForgotPassword = () => {
 
     sendPasswordResetEmail(auth, email).catch((e) => {
       console.dir(e);
-      alert(firebaseErrors[e.code]);
+      setShow(true);
+      // alert(firebaseErrors[e.code]);
     });
   };
 
   return (
-    <Form
-      submitText="Poproś o przypomnienie hasła"
-      onSubmit={handlePasswordReset}
-      isPasswordHidden
-    />
+    <>
+      <Form
+        submitText="Poproś o przypomnienie hasła"
+        onSubmit={handlePasswordReset}
+        isPasswordHidden
+      />
+      <RegisterModal show={show} setShow={setShow} />;
+    </>
   );
 };
