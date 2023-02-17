22f2d68c321f67526f9728863275f24e3f0bacc6
Maciej Walentynowicz <maciej.walentynowicz@gmail.com>
2023-02-17 16:47:47 +0100

    made hotfix to css in modals and forgotpassword section

--- fitpals/src/components/Modals/ForgotPasswordModal/ForgotPasswordModal.jsx
+++ fitpals/src/components/Modals/ForgotPasswordModal/ForgotPasswordModal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./ForgotPasswordModal.module.css";
 import Logo from "../../Images/FitPalLogo.png";
+import CancelButton from "../../../Assets/cancel button.svg";
 
 export const ForgotPasswordModal = ({ show, setShow }) => {
   const content = show && (
@@ -10,7 +11,11 @@ export const ForgotPasswordModal = ({ show, setShow }) => {
         type="button"
         onClick={() => setShow(false)}
       >
-        X
+        <img
+          src={CancelButton}
+          alt="cancel button"
+          className={styles.cancelButton}
+        />
       </button>
       <h1>Za chwilę otrzymasz maila do resetu hasła !</h1>
       <img src={Logo} alt="logo of company" className={styles.logo} />
