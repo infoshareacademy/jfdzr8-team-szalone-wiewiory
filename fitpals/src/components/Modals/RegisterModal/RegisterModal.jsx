24c907eb4a58543c2f83ba58878584e827fb73a4
Olga Grochowska <103778400+olga-grochowska@users.noreply.github.com>
2023-02-18 01:19:47 +0100

    NavLinks in Auth to redirect to another component

--- fitpals/src/components/Modals/RegisterModal/RegisterModal.jsx
+++ fitpals/src/components/Modals/RegisterModal/RegisterModal.jsx
@@ -2,25 +2,28 @@ import React from "react";
 import styles from "./RegisterModal.module.css";
 import Logo from "../../Images/FitPalLogo.png";
 import CancelButton from "../../../Assets/cancel button.svg";
+import { NavLink } from "react-router-dom";
 
 export const RegisterModal = ({ show, setShow }) => {
-  const content = show && (
-    <div className={styles.modal}>
-      <button
-        className={styles.modalClose}
-        type="button"
-        onClick={() => setShow(false)}
-      >
-        <img
-          src={CancelButton}
-          alt="cancel button"
-          className={styles.cancelButton}
-        />
-      </button>
-      <h1>Gratulacje, zostałeś poprawnie zarejestrowany !</h1>
-      <img src={Logo} alt="logo of company" className={styles.logo} />
-    </div>
+  return (
+    <NavLink to="/login">
+      {show ? (
+        <div className={styles.modal}>
+          <button
+            className={styles.modalClose}
+            type="button"
+            onClick={() => setShow(false)}
+          >
+            <img
+              src={CancelButton}
+              alt="cancel button"
+              className={styles.cancelButton}
+            />
+          </button>
+          <h1>Gratulacje, zostałeś poprawnie zarejestrowany !</h1>
+          <img src={Logo} alt="logo of company" className={styles.logo} />
+        </div>
+      ) : null}
+    </NavLink>
   );
-
-  return content;
 };
